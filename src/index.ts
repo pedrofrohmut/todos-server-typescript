fd5050bdf9dd75682acfa61f5a0b573ba6fd9a8a
Pedro Frohmut <pedrofrohmut@gmail.com>
2020-07-15 14:35:46 -0300

    Database Connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
 
+import { connectDatabase } from "./config/db"
+
+// Setup enviroment variables
 dotenv.config({ path: "./.env" })
+
+connectDatabase()
 
 // Express initialization
 const app = express()
 
-const PORT = process.env.PORT || 5001
+/**
+ * Middlewares
+ */
+app.use(express.json())
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5000
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
